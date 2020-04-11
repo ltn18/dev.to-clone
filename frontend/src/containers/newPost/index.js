bc1ed27312dee851fcd8659d4eaf88c82f88c09f
HOANGLAM2952002 <tommy2952002@gmail.com>
2020-04-11 16:36:39 +0700

    use hoc & hof

--- frontend/src/containers/newPost/index.js
+++ frontend/src/containers/newPost/index.js
@@ -1,47 +1,9 @@
-import React, { useContext, useEffect, useCallback } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
-import axios from '../../config/axios';
-import { useAsync } from '../../hooks/useAsync';
-
-import Auth from '../auth';
-import authCtx from '../../contexts/auth'
+import withAuth from '../../hoc/authHoc';
 
 const NewPost = () => {
-  const { authUser } = useContext(authCtx);
-
-  const [profileApiData, fetchProfile] = useAsync(
-    {},
-    useCallback(
-      (token) =>
-        axios.get(
-          "http://localhost:5000/me",
-          {
-            headers: {
-              Authorization: "Bearer " + token,
-            }
-          }
-        )
-    )
-  );
-
-
-  useEffect(() => {
-    const jwt = localStorage.getItem("jwt");
-    if (jwt) {
-      // Step 1: login with token
-      // Step 2: get userProfile
-      // Step 3: update authUser context with user
-      fetchProfile(jwt);
-    }
-  }, []) // fetchProfile 1 lần, nếu để thành dependency sẽ load đến lag :v
-
-  if (!authUser) return <Auth />;
-
-  return (
-    <Container>
-      Write new post
-    </Container>
-  )
+  return (<Container>Write new post</Container>)
 }
 
-export default NewPost
+export default withAuth(NewPost); // using higher order function;
--- frontend/src/hoc/authHoc.js
+++ frontend/src/hoc/authHoc.js
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react';
 import authCtx from '../contexts/auth';
-
+import Auth from '../containers/auth/';
 
 // một function return ra một function khác
 // nhận vào một component và return một component khác
-const withAuth = (Component) => () => {
+const withAuth = (Component) => (props) => {
   const { authUser } = useContext(authCtx);
-  if (authUser) {
-    return <Component />
-  } 
-  return <div>User must login to perform this task!</div>;
+  if (!authUser) {
+    const jwt = localStorage.getItem("jwt");
+    if (jwt) {
+
+    } else {
+      return <Auth {...props} />;
+    }
+  }
+  return <Component {...props} /> // 2 props truyền vào có giống nhau ko?
 }
+
+export default withAuth;
